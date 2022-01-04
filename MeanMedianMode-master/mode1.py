f89b23d050a257dc17e6db759e2408438c470418
Taha123873 <85600431+Taha123873@users.noreply.github.com>
2022-01-04 13:25:19 +0530

    Add files via upload

---
+++ MeanMedianMode-master/mode1.py
@@ -0,0 +1,40 @@
+# Python program to print
+# mode of elements
+from collections import Counter
+import csv
+
+with open('height-weight.csv', newline='') as f:
+    reader = csv.reader(f)
+    file_data = list(reader)
+
+file_data.pop(0)
+
+new_data=[]
+for i in range(len(file_data)):
+	n_num = file_data[i][2]
+	new_data.append(n_num)
+
+
+
+#Calculating Mode
+data = Counter(new_data)
+
+mode_data_for_range = {
+                        "90-100": 0,
+                        "100-110": 0,
+                        "110-120": 0
+                    }
+for height, occurence in data.items():
+    if 50 < float(height) < 60:
+        mode_data_for_range["90-100"] += occurence
+    elif 60 < float(height) < 70:
+        mode_data_for_range["100-110"] += occurence
+    elif 70 < float(height) < 80:
+        mode_data_for_range["110-120"] += occurence
+
+mode_range, mode_occurence = 0, 0
+for range, occurence in mode_data_for_range.items():
+    if occurence > mode_occurence:
+        mode_range, mode_occurence = [int(range.split("-")[0]), int(range.split("-")[1])], occurence
+mode = float((mode_range[0] + mode_range[1]) / 2)
+print(f"Mode is -> {mode:2f}")
